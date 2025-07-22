e6e1926bd500064042227be04d291ed6b40fa15b
Verseler <vrslr2003@gmail.com>
2025-07-22 22:02:57 +0800

    fix: what header button to render based on user role

--- resources/js/components/MainHeader.tsx
+++ resources/js/components/MainHeader.tsx
@@ -4,10 +4,13 @@ import LinkButton from "@/components/LinkButton";
 import { Input } from "@/components/ui/input";
 import UserAvatar from "@/features/auth/components/UserAvatar";
 import { useCart } from "@/features/cart/context/CartContext";
+import { PageProps } from "@/types";
+import { usePage } from "@inertiajs/react";
 import { ShoppingCartIcon } from "lucide-react";
 
 export default function MainHeader() {
     const { productCartCount } = useCart();
+    const roles = usePage<PageProps>().props?.auth?.user?.roles;
 
     return (
         <nav className="fixed z-10 w-full border-b bg-primary-50">
@@ -23,7 +26,13 @@ export default function MainHeader() {
                         <span className="mt-1 -ms-0.5">{productCartCount}</span>
                     </LinkButton>
 
-                    <UserAvatar />
+                    {roles?.includes("admin") ? (
+                        <LinkButton href={route("dashboard")}>
+                            Dashboard
+                        </LinkButton>
+                    ) : (
+                        <UserAvatar />
+                    )}
                 </div>
             </Container>
         </nav>
--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -1,8 +1,11 @@
+export type UserRole = 'admin' | 'customer';
+
 export interface User {
     id: number;
     name: string;
     email: string;
     email_verified_at?: string;
+    roles: UserRole[];
 }
 
 export type PageProps<
