4f1026a0719a137f9a7e67ca66d978aca0d15f5f
Verseler <vrslr2003@gmail.com>
2025-08-01 18:40:39 +0800

    new searchbox for product list

---
+++ resources/js/features/product/components/ProductSearchbox.tsx
@@ -0,0 +1,39 @@
+import { SearchIcon } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { useForm, usePage } from "@inertiajs/react";
+import { PageProps } from "@/types";
+import { ProductFilters } from "@/features/product/product.types";
+import { useEffect } from "react";
+import { useDebouncedCallback } from "use-debounce";
+
+export default function ProductSearchbox() {
+    const { search } = usePage<PageProps & ProductFilters>().props;
+
+    //form for applying search filter
+    const { get, data, setData } = useForm<ProductFilters>({
+        search: search ?? "",
+    });
+
+    const handleSearch = useDebouncedCallback(() => {
+        get(route("products.index"), {
+            preserveState: true,
+            preserveScroll: true,
+            replace: true,
+        });
+    }, 1000);
+
+    useEffect(() => {
+        handleSearch();
+    }, [data.search]);
+
+    return (
+        <div className="relative ml-auto max-w-60">
+            <SearchIcon className="absolute top-3 left-2 size-4 text-neutral-500" />
+            <Input
+                value={data.search}
+                onChange={(e) => setData("search", e.target.value)}
+                className="pl-8 rounded-md"
+            />
+        </div>
+    );
+}
